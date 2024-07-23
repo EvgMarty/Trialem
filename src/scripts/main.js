8b13a8d547906a670b74f1aec0a26302c4ccbbb1
EvgMarty <mrtevgeniy@gmail.com>
2024-07-23 20:56:19 +0300

    created new sections - why, feedback and request

---
+++ src/scripts/main.js
@@ -0,0 +1,9 @@
+// swiper
+const swiper = new Swiper('.feedback .swiper', {
+  slidesPerView: 2,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-btn--next',
+    prevEl: '.slider-btn--prev',
+  },
+});

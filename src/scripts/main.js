0c7d83023623ddeec5f3c0800eb4789f7805dc69
EvgMarty <mrtevgeniy@gmail.com>
2024-07-24 20:34:46 +0300

    fix header nav

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,15 @@
-// swiper
+// fixed header
+const headerEl = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > headerEl.offsetHeight) {
+    headerEl.classList.add('scrolled');
+  } else {
+    headerEl.classList.remove('scrolled');
+  }
+});
+
+// simple swiper slider whith2 card
 const swiper = new Swiper('.feedback .swiper', {
   slidesPerView: 2,
   // Navigation arrows
